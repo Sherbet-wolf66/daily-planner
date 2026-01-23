f21ad116625530220c05c8eeba41150cc43a9625
Sherbet Wolf <obris673@hsd.k12.or.us>
2026-01-23 09:29:36 -0800

    Git commit1

---
+++ src/daily_planner/daily_planner.py
@@ -0,0 +1,12 @@
+"""
+Daily Planner module
+by Sherry
+"""
+
+
+def main():
+    print("welcome")
+
+
+if __name__ =="__main__":
+    main()
---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+
+git config --global user.email "obris673@hsd.k12.or.us"
+git config --global user.name "Sherbetwolf"
